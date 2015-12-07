1351f450170d4d323b0525e12c5428f97ba2dd2e
Hien Luu <hluu@linkedin.com>
2015-12-07 11:26:20 -0800

    added the implementation for MaxConcurrentEvents

--- src/main/java/my/epi/sorting/MaxConcurrentEvents.java
+++ src/main/java/my/epi/sorting/MaxConcurrentEvents.java
@@ -2,20 +2,58 @@ package my.epi.sorting;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 /**
  * Created by hluu on 12/7/15.
  *
  * Problem statement:
+ * Given a set of potentially overlapped intervals where each has
+ * a start and end point, determine the maximum number of concurrent
+ * intervals.  Concurrent intervals are defined as overlapping, where
+ * the start and end of an interval is between the start or the end of
+ * another interval.
  *
  * Approach:
  *
- * Runtime Analysis
+ * Naive:
+ *   Since the set of intervals are not sorted, we need to go through the
+ *   list for each of the interval therefore run time will be O(n^2)
  *
+ * Better:
+ *   Sort the list of interval by starting point.  For intervals that have
+ *   same starting point, sort them by end point.
+ *
+ *   Imagine lining up the interval based on the starting point and move
+ *   a vertical line through this sorted list.  The intersection of the
+ *   number of intervals represents # of concurrent intervals.  So now
+ *   the question is how to keep track of this count in the code.
+ *
+ *   Imagine below
+ *
+ *   |-------------| (1)
+ *         |------------| (2)
+ *                        |----------| (3)
+ *   ===================================
+ *
+ *   Interval #1 and 2 are overlapped and interval #3 is not overlapped with any other
+ *   intervals.  In this simple example, the max concurrent # of intervals is 2.
+ *
+ *   The general technique is to keep a counter where it is incremented when starting an
+ *   interval and decremented when ending an interval.  Additional max concurrent interval
+ *   counter is needed and updated when the counter is incremented.
+ *
+ *   This approach requires two steps:
+ *     1) Sort the intervals by start O(nlogn)
+ *     2) Iterate through the sorted interval O(n)
+ *
+ *
+ *
+ * Runtime Analysis:
+ *   The runtime for the better approach is O(nlogn)
  *
  * Memory Analysis:
+ *   We only need two variables  and they are independent of n, so it would be O(1)
  */
 public class MaxConcurrentEvents {
     public static void main(String[] args) {
@@ -25,14 +63,43 @@ public class MaxConcurrentEvents {
         intervalList.add(new Interval(1,5));
         intervalList.add(new Interval(5,10));
         intervalList.add(new Interval(3,7));
+        intervalList.add(new Interval(5,8));
 
-        System.out.println(intervalList);
-        Collections.sort(intervalList);
-        System.out.println("After sorting: " + intervalList);
+
+
     }
 
+    public static int maxConcurrentIntervals(List<Interval> input) {
+        System.out.println(input);
+        Collections.sort(input);
+        System.out.println("After sorting: " + input);
 
-    private static class Interval implements Comparable<Interval> {
+        int maxConcurrIntervalCounter = 0;
+        int counter = 0;
+
+        Interval prevInterval = null;
+        for (Interval intv : input) {
+            if (prevInterval == null) {
+                prevInterval = intv;
+                counter++;
+                maxConcurrIntervalCounter = counter;
+            } else if (intv.start <= prevInterval.end) {
+                counter++;
+                if (counter > maxConcurrIntervalCounter) {
+                    maxConcurrIntervalCounter = counter;
+                }
+                // maintain the internval the largest end point
+                prevInterval = (intv.end > prevInterval.end) ? intv : prevInterval;
+            } else {
+                // new interval starts after the end of prevInternval
+                prevInterval = intv;
+                counter = 1;
+            }
+        }
+        return maxConcurrIntervalCounter;
+    }
+
+    public static class Interval implements Comparable<Interval> {
         public int start;
         public int end;
 
--- src/test/java/my/epi/sorting/MaxConcurrentEventsTest.java
+++ src/test/java/my/epi/sorting/MaxConcurrentEventsTest.java
@@ -1,6 +1,11 @@
 package my.epi.sorting;
 
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 /**
@@ -9,7 +14,57 @@ import static org.testng.Assert.assertTrue;
 public class MaxConcurrentEventsTest {
 
     @Test
-    public void basicTest() {
-        assertTrue(true);
+    public void oneOverlappingTest() {
+        List<MaxConcurrentEvents.Interval> intervalList = new ArrayList<MaxConcurrentEvents.Interval>();
+        intervalList.add(new MaxConcurrentEvents.Interval(1,5));
+        intervalList.add(new MaxConcurrentEvents.Interval(6,10));
+        assertEquals(MaxConcurrentEvents.maxConcurrentIntervals(intervalList), 1);
+    }
+
+    @Test
+    public void twoOverlappingTest() {
+        List<MaxConcurrentEvents.Interval> intervalList = new ArrayList<MaxConcurrentEvents.Interval>();
+        intervalList.add(new MaxConcurrentEvents.Interval(1,5));
+        intervalList.add(new MaxConcurrentEvents.Interval(2,7));
+        intervalList.add(new MaxConcurrentEvents.Interval(8,10));
+        assertEquals(MaxConcurrentEvents.maxConcurrentIntervals(intervalList), 2);
+    }
+
+    @Test
+    public void twoOverlappingSamePointTest() {
+        List<MaxConcurrentEvents.Interval> intervalList = new ArrayList<MaxConcurrentEvents.Interval>();
+        intervalList.add(new MaxConcurrentEvents.Interval(1,5));
+        intervalList.add(new MaxConcurrentEvents.Interval(5,7));
+        intervalList.add(new MaxConcurrentEvents.Interval(8,10));
+        assertEquals(MaxConcurrentEvents.maxConcurrentIntervals(intervalList), 2);
+    }
+
+    @Test
+    public void twoOverlappingShortWithinLongIntervalTest() {
+        List<MaxConcurrentEvents.Interval> intervalList = new ArrayList<MaxConcurrentEvents.Interval>();
+        intervalList.add(new MaxConcurrentEvents.Interval(3,5));
+        intervalList.add(new MaxConcurrentEvents.Interval(1,7));
+        intervalList.add(new MaxConcurrentEvents.Interval(8,10));
+        assertEquals(MaxConcurrentEvents.maxConcurrentIntervals(intervalList), 2);
+    }
+
+    @Test
+    public void threeOverlappingTest() {
+        List<MaxConcurrentEvents.Interval> intervalList = new ArrayList<MaxConcurrentEvents.Interval>();
+        intervalList.add(new MaxConcurrentEvents.Interval(1,5));
+        intervalList.add(new MaxConcurrentEvents.Interval(2,7));
+        intervalList.add(new MaxConcurrentEvents.Interval(4,10));
+        intervalList.add(new MaxConcurrentEvents.Interval(15,20));
+        assertEquals(MaxConcurrentEvents.maxConcurrentIntervals(intervalList), 3);
+    }
+
+    @Test
+    public void threeOverlappingOneLongOneTest() {
+        List<MaxConcurrentEvents.Interval> intervalList = new ArrayList<MaxConcurrentEvents.Interval>();
+        intervalList.add(new MaxConcurrentEvents.Interval(1,13));
+        intervalList.add(new MaxConcurrentEvents.Interval(2,7));
+        intervalList.add(new MaxConcurrentEvents.Interval(4,10));
+        intervalList.add(new MaxConcurrentEvents.Interval(15,20));
+        assertEquals(MaxConcurrentEvents.maxConcurrentIntervals(intervalList), 3);
     }
 }
