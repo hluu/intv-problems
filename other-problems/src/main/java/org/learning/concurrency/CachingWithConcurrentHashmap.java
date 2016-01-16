1235bffc1b48f6678bda1deccf9a162cf7d20ede
Hien Luu <hluu@linkedin.com>
2016-01-16 15:43:41 -0800

    concurrency

---
+++ other-problems/src/main/java/org/learning/concurrency/CachingWithConcurrentHashmap.java
@@ -0,0 +1,103 @@
+package org.learning.concurrency;
+
+import java.util.concurrent.*;
+
+/**
+ * Created by hluu on 1/14/16.
+ *
+ * Problem:
+ *  Create a cache with good concurrency and make sure the
+ *  expensive computation is not recomputed twice in the case
+ *  of race condition.
+ *
+ * Approach:
+ *  Using a combination of ConcurrentHashMap, FutureTask, Future
+ *
+ */
+public class CachingWithConcurrentHashmap<K,V> {
+
+    private ConcurrentHashMap<K,FutureTask<V>> cache = new ConcurrentHashMap<>();
+
+    public CachingWithConcurrentHashmap() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("CachingWithConcurrentHashmap.main");
+
+        CachingWithConcurrentHashmap<String,Integer> cache = new CachingWithConcurrentHashmap<String,Integer>();
+
+        Integer prevValue = cache.put("One", 1);
+        prevValue = cache.put("Two", 2);
+
+        System.out.println("One: " + cache.get("One"));
+        System.out.println("Three: " + cache.get("Three"));
+    }
+
+    public V put(K key, V value) {
+        Future<V> resultHolder =  cache.put(key, new LocalFutureTask(value));
+        try {
+            return resultHolder.get();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * The main point here is storing a Future inside cache and leverage
+     * atomic operation ConcurrentHashmap.putIfAbsent()
+     *
+     * @param key
+     * @return
+     */
+    public V get(K key) {
+        Future<V> resultHolder = cache.get(key);
+
+        if (resultHolder == null) {
+            FutureTask<V> ft = new FutureTask<V>(new LocalCallable(key));
+            cache.putIfAbsent(key, ft);
+            ft.run();
+            resultHolder = ft;
+        }
+        try {
+            V result = resultHolder.get();
+            return  result;
+        } catch (Exception e) {
+            throw new RuntimeException("Something went wrong");
+        }
+    }
+
+    private static class LocalCallable<T> implements Callable<T> {
+        private T value;
+       public LocalCallable(T value) {
+           this.value = value;
+       }
+        public T call() throws Exception {
+            return value;
+        }
+    }
+
+    private static class DummyCallable<V> implements Callable<V> {
+        private V v;
+        public DummyCallable(V v) {
+            this.v = v;
+        }
+
+        public V call() throws Exception {
+            return v;
+        }
+    }
+
+    private static class LocalFutureTask<V> extends FutureTask<V> {
+        private V value;
+
+        public LocalFutureTask(V v) {
+            super(new DummyCallable<V>(v));
+            this.value = v;
+        }
+
+        @Override
+        public V get() throws InterruptedException, ExecutionException {
+            return value;
+        }
+    }
+}
---
+++ other-problems/src/main/java/org/learning/concurrency/CookieJar.java
@@ -0,0 +1,59 @@
+package org.learning.concurrency;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * Created by hluu on 1/14/16.
+ *
+ * Problem:
+ *  A cookie jar provides cookie consumers to enjoy cookies and cookie bakers to
+ *  add cookies to the jar.
+ *
+ *  Develop a CookieJar to hold cookies such that cookies consumers have the ability to
+ *  take cookies to enjoy without waiting on each other
+ */
+public class CookieJar {
+    private ReadWriteLock rwLock = new ReentrantReadWriteLock();
+    private Lock readLock = rwLock.readLock();
+    private Lock writeLock = rwLock.writeLock();
+
+    private volatile  int count;
+    public CookieJar(int initialNumCookies) {
+        this.count = initialNumCookies;
+    }
+
+    public Cookie takeCookie() {
+        try {
+            readLock.lock();
+            count--;
+            return new Cookie();
+        } finally {
+             readLock.unlock();;
+        }
+    }
+
+    public void addCookie(Cookie cookie) {
+        try {
+            writeLock.lock();
+            count++;
+        } finally {
+            writeLock.unlock();;
+        }
+    }
+
+    private static class Cookie {
+        public Cookie() {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("CookieJar.main");
+    }
+
+}
+
+
+
