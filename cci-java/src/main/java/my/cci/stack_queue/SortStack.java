ad10d0a7a5a6b47e8bc00e4b3ca2b1ac11a2c20f
Hien Luu <hluu@linkedin.com>
2016-01-17 15:23:50 -0800

    sorting a stack

---
+++ cci-java/src/main/java/my/cci/stack_queue/SortStack.java
@@ -0,0 +1,89 @@
+package my.cci.stack_queue;
+
+import java.util.Stack;
+
+/**
+ * Created by hluu on 1/17/16.
+ *
+ * Problem statement:
+ *  Give a stack of integers that are not sorted.  Develop an algorithm to sort the
+ *  stack.
+ *
+ *  Constraints:
+ *      1) Can't use any additional data structure exception stack
+ *
+ *
+ * Approach:
+ *  We are limited to just stack data structure.  This seems to suggest we need at least
+ *  two stacks.
+ *
+ *  Let's say we use two stacks - original one and the additional one.  The second
+ *  stack will store the elements in sorted order where the large values are at the
+ *  bottom.  We can leverage the insert sort logic, where we start with empty stack.
+ *  When we want to insert a new value, we will need to find the correct spot to insert
+ *  it popping out the values that are smaller than it.  Once the new value is in the correct
+ *  place, we then push back those smaller values.
+ *
+ *
+ *  For example:
+ *     input = [1,5,2,3,4]
+ *
+ *     tmp = 1, input: [5,2,3,4] =>  output = [1]
+ *     tmp = 5, input: [2,3,4]   =>  output = [5], input: [1,2,3,4]
+ *     tmp = 1, input: [2,3,4]   => output[1,5]
+ *     tmp = 2, input: [3,4]     => output[2,5], input: [1,3,4]
+ *     tmp = 1, input: [3,4]     => output[1,2,5]
+ *     tmp = 3, input: [4]       => output[3,5], input[1,2,4]
+ *
+ *  This is O(n^2) runtime complexity
+ *
+ */
+public class SortStack {
+    public static void main(String[] args) {
+        System.out.println("SortStack.main");
+
+        Stack<Integer> input = new Stack<Integer>();
+        input.push(4);
+        input.push(3);
+        input.push(2);
+        input.push(5);
+        input.push(1);
+        input.push(10);
+        input.push(9);
+
+        System.out.println("before sorting: " + input);
+        Stack<Integer> output = sort(input);
+
+        System.out.println("after sorting");
+
+        boolean first = false;
+        while (!output.isEmpty()) {
+            if (first) {
+                System.out.printf(", ");
+            }
+            first=true;
+            System.out.printf("%d", output.pop());
+        }
+        System.out.println();
+    }
+
+    public static Stack<Integer> sort(Stack<Integer> input) {
+        Stack<Integer> output = new Stack();
+
+        Integer tmp = null;
+
+        while (!input.isEmpty()) {
+            tmp = input.pop();
+
+            // find the correct spot to push, meaning popping out all smaller values
+            // or until no more element
+            while (!output.isEmpty() && output.peek() < tmp) {
+                input.push(output.pop());
+            }
+
+            output.push(tmp);
+
+        }
+        return output;
+    }
+}
---
+++ cci-java/src/test/java/my/cci/stack_queue/SortStackTest.java
@@ -0,0 +1,77 @@
+package my.cci.stack_queue;
+
+import org.apache.tools.ant.types.resources.Sort;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Random;
+import java.util.Stack;
+
+/**
+ * Created by hluu on 1/17/16.
+ */
+public class SortStackTest {
+    @Test
+    public void oneElemStack() {
+        Stack<Integer> input = new Stack<Integer>();
+        input.push(4);
+
+        Stack<Integer> output = SortStack.sort(input);
+        Assert.assertEquals(output.size(), 1);
+        Assert.assertEquals(output.peek().intValue(), 4);
+
+        validateSortedStack(output);
+    }
+
+    @Test
+    public void twoElemStack() {
+        Stack<Integer> input = new Stack<Integer>();
+        input.push(4);
+        input.push(5);
+
+        Stack<Integer> output = SortStack.sort(input);
+        Assert.assertEquals(output.size(), 2);
+        Assert.assertEquals(output.peek().intValue(), 4);
+
+        validateSortedStack(output);
+    }
+
+    @Test
+    public void threeElemStack() {
+        Stack<Integer> input = new Stack<Integer>();
+        input.push(4);
+        input.push(1);
+        input.push(5);
+
+        Stack<Integer> output = SortStack.sort(input);
+        Assert.assertEquals(output.size(), 3);
+        Assert.assertEquals(output.peek().intValue(), 1);
+
+        validateSortedStack(output);
+    }
+
+    @Test
+    public void multipleElemStack() {
+        Stack<Integer> input = new Stack<Integer>();
+        Random random = new Random(System.currentTimeMillis());
+        int numElm = 20;
+        for (int i = 0; i < numElm; i++) {
+            input.push(random.nextInt(2500));
+        }
+
+        // System.out.println(input);
+        Stack<Integer> output = SortStack.sort(input);
+        //System.out.println(output);
+        validateSortedStack(output);
+    }
+
+    private void validateSortedStack(Stack<Integer> input) {
+        // validate each subsequent elements are greater than or equal to previous value
+        Integer prev = input.pop();
+
+        while (!input.isEmpty()) {
+            Assert.assertTrue(prev.intValue() <= input.peek().intValue());
+            prev = input.pop();
+        }
+    }
+}
