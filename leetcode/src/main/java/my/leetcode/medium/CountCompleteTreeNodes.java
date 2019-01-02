80761254b3f6f60bce6829b5319d39673d90db5f
hluu <hluu@yahoo.com>
2019-01-02 15:08:43 -0800

    count complete tree nodes

---
+++ leetcode/src/main/java/my/leetcode/medium/CountCompleteTreeNodes.java
@@ -0,0 +1,169 @@
+package my.leetcode.medium;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given a complete binary tree, count the number of nodes.
+ *
+ * Note:
+ *
+ * Definition of a complete binary tree from Wikipedia:
+ * In a complete binary tree every level, except possibly the last,
+ * is completely filled, and all nodes in the last level are as far left
+ * as possible.
+ *
+ * It can have between 1 and 2h nodes inclusive at the last level h.
+ *
+ * Example:
+ * Input:
+ *        1
+ *       / \
+ *      2   3
+ *     / \  /
+ *    4  5 6
+ *
+ * Output: 6
+ *
+ */
+public class CountCompleteTreeNodes {
+
+    public static void main(String[] args) {
+        System.out.println(CountCompleteTreeNodes.class.getName());
+
+        test(createSmallTree(), 6);
+    }
+
+    private static void test(TreeNode root, int expected) {
+        int actual = countNodes(root);
+        int actual2 = countNodes2(root);
+        int actual3 = countNodes3(root);
+
+        System.out.printf("expected: %d, actual: %d, actual2: %d, actual2: %d\n",
+                expected, actual, actual2, actual3);
+
+
+    }
+
+    private static TreeNode createSmallTree() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        root.right.left = new TreeNode(6);
+        //root.right.right = new TreeNode(7);
+
+        return root;
+
+
+    }
+
+    private static int countNodes2(TreeNode root) {
+        // base case
+        if (root == null) {
+            return 0;
+        }
+
+        int height = 0;
+        TreeNode left = root.left, right = root.right;
+
+        // figuring out the height in one short instead of call
+        // going left or going right
+        while (left != null && right != null) {
+            height++;
+            left = left.left;
+            right = right.right;
+        }
+        if (left == null && right == null) return (1 << (height+1)) - 1;
+        else return 1 + countNodes2(root.left) + countNodes2(root.right);
+    }
+
+    private static int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = countNodesHelper(root.left);
+        int right = countNodesHelper(root.right);
+
+        return 1 + left + right;
+    }
+
+    private static int countNodesHelper(TreeNode node) {
+
+        if (node == null) {
+            return 0;
+        }
+
+        int leftLevel = goingLeft(node);
+        int rightLevel = goingRight(node);
+
+        // leveraging the full-tree property
+        if (leftLevel == rightLevel) {
+            // if it is a full tree
+            return (int)Math.pow(2, leftLevel) - 1;
+        }
+
+        return 1 + countNodesHelper(node.left) +  countNodesHelper(node.right);
+
+    }
+
+    public static int countNodes3(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(root);
+        int count=1;
+        while(!q.isEmpty()){
+            TreeNode temp = q.poll();
+            if(temp.val!=-1000){          // HERE!! other negative integers work as well
+                temp.val=-1000;
+
+                if(temp.left!=null){
+                    q.offer(temp.left);
+                    count++;
+                }
+
+                if(temp.right!=null){
+                    q.offer(temp.right);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private static int goingLeft(TreeNode node) {
+        int level = 0;
+
+        while (node != null) {
+            level++;
+            node = node.left;
+        }
+
+        return level;
+    }
+
+    private static int goingRight(TreeNode node) {
+        int level = 0;
+
+        while (node != null) {
+            level++;
+            node = node.right;
+        }
+
+        return level;
+    }
+
+    private static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+    }
+}
--- leetcode/src/main/java/my/leetcode/medium/MaximumLenPairChain.java
+++ leetcode/src/main/java/my/leetcode/medium/MaximumLenPairChain.java
@@ -1,5 +1,7 @@
 package my.leetcode.medium;
 
+import org.junit.Assert;
+
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -19,18 +21,24 @@ import java.util.Comparator;
  *
  * Brute force:
  * - if the pairs are sorted by the first element, then therefore the
- *   second elments are sorted.
+ *   second elements are sorted.
  *
  * - for each pair in the input, treat that as the beginning of the chain
  *   search through remaining pairs
  *   - chain them if possible and keep running count
  *   - if not, move to next unchained pairs
  *
+ * Dynamic programming:
+ *  - optimal subproblem
+ *    If a chain of length k ends at some pairs[i], and pairs[i][1] < pairs[j][0],
+ *    we can extend this chain to a chain of length k+1.
+ *  - overlapping subproblem
+ *
  */
 public class MaximumLenPairChain {
     public static void main(String[] args) {
 
-       // test(new int[][] {{1,2}, {2,3}, {3,4}}, 2);
+        test(new int[][] {{1,2}, {2,3}, {3,4}}, 2);
         test(new int[][] {{3,4}, {1,2}, {2,3} }, 2);
         test(new int[][] {{-10,-8},{8,9},{-5,0},{6,10},{-6,-4},{1,7},{9,10},{-4,7}}, 4);
         test(new int[][] {{7,9},{4,5},{7,9},{-7,-1},{0,10},{3,10},{3,6},{2,3}}, 4);
@@ -39,12 +47,24 @@ public class MaximumLenPairChain {
 
     private static void test(int[][] input, int expected) {
 
-        int actual = findLongestChain(input);
-        System.out.printf("expected: %d, actual: %d\n",
+        int actual = findLongestChainBUDP(input);
+        System.out.printf("expected: %d, actual: %d ",
                 expected, actual);
+
+        Assert.assertEquals(expected, actual);
+
+        int actual2 = findLongestChainOther(input);
+        System.out.printf("actual2: %d ", actual2);
+
+        Assert.assertEquals(expected, actual2);
+
+        int actualDP2 = findLongestChainDP2(input);
+        System.out.printf("actualDP2: %d\n", actualDP2);
+
+        Assert.assertEquals(expected, actualDP2);
     }
 
-    private static int findLongestChain(int[][] pairs) {
+    private static int findLongestChainBUDP(int[][] pairs) {
 
         // this is pretty cool about sorting an array
         Arrays.sort(pairs, Comparator.comparingInt(a -> a[0]));
@@ -57,6 +77,7 @@ public class MaximumLenPairChain {
         int maxCount = 0;
 
         int[] countCache = new int[pairs.length];
+        // every single element is a chain of 1
         Arrays.fill(countCache,1);
         //System.out.printf("countCache: %s\n", Arrays.toString(countCache));
 
@@ -68,6 +89,7 @@ public class MaximumLenPairChain {
             }
 
             if (prevIdx >= 0) {
+                // do this only when inside the boundary of first element
                 countCache[curIdx] = Math.max(countCache[curIdx], countCache[prevIdx]) + 1;
             }
             /*
@@ -81,5 +103,65 @@ public class MaximumLenPairChain {
         }
 
         return maxCount;
+    }
+
+    /**
+     * Runtime: O(n^2), space: O(n)
+     * @param pairs
+     * @return
+     */
+    public static int findLongestChainDP2(int[][] pairs) {
+        Arrays.sort(pairs, (a, b) -> a[0] - b[0]);
+
+        int N = pairs.length;
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+
+        //(0,1), (0,2), (0,3), (0,4)... (0,N)
+        for (int j = 1; j < N; ++j) {
+            // going from 0 to j
+            for (int i = 0; i < j; ++i) {
+                if (pairs[i][1] < pairs[j][0])
+                    // update dp[j]
+                    dp[j] = Math.max(dp[j], dp[i] + 1);
+            }
+        }
+
+        int ans = 0;
+        for (int x: dp) {
+            if (x > ans) ans = x;
+        }
+        return ans;
+    }
+
+
+    /**
+     * This approach sorts the pairs by the second element in the array.
+     *
+     * @param pairs
+     * @return
+     */
+    public static int findLongestChainOther(int[][] pairs) {
+        if(pairs.length == 0) return 0;
+        Arrays.sort(pairs, (a,b)->a[1]-b[1]);
+
+        System.out.printf("\n*** findLongestChainOther ***\n");
+        for (int[] pair : pairs) {
+            System.out.printf("%s, ", Arrays.toString(pair));
+        }
+
+        int prevB = pairs[0][1];
+        int count = 1;
+
+        for(int i = 1 ; i < pairs.length; i++){
+            int nextC = pairs[i][0];
+            int nextD = pairs[i][1];
+
+            if(nextC > prevB){
+                count++;
+                prevB = nextD;
+            }
+        }
+        return count;
     }
 }
